07d1f494c024a5b19b071b3d0c15c1f8d09f6db5
GabyCliff <fernandezgabriel517@gmail.com>
2020-09-26 21:15:34 -0300

    delete de vistas en thymeleaf innecesarias

--- src/main/java/com/wchallenge/controllers/AlbumController.java
+++ src/main/java/com/wchallenge/controllers/AlbumController.java
@@ -4,14 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.wchallenge.helpers.ViewRouteHelper;
 import com.wchallenge.model.AlbumModel;
 import com.wchallenge.service.IAlbumService;
 
@@ -21,13 +17,6 @@ public class AlbumController {
 	
 	@Autowired
 	private IAlbumService albumService;
-	
-//	@GetMapping("")
-//    public ModelAndView getAlbums(){
-//        ModelAndView mAV = new ModelAndView(ViewRouteHelper.ALBUM_INDEX);
-//        mAV.addObject("albums", albumService.getAll());
-//        return mAV;
-//    }
 	
 	@GetMapping("")
 	public List<AlbumModel> getAlbums() {
--- src/main/java/com/wchallenge/controllers/PhotoController.java
+++ src/main/java/com/wchallenge/controllers/PhotoController.java
@@ -4,14 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.wchallenge.helpers.ViewRouteHelper;
 import com.wchallenge.model.PhotoModel;
 import com.wchallenge.service.IPhotoService;
 
@@ -21,13 +18,6 @@ public class PhotoController {
 	
 	@Autowired
 	private IPhotoService photoService;
-	
-//	@GetMapping("")
-//    public ModelAndView getPhotos(){
-//        ModelAndView mAV = new ModelAndView(ViewRouteHelper.PHOTO_INDEX);
-//        mAV.addObject("photos", photoService.getAll());
-//        return mAV;
-//    }
 	
 	@GetMapping("")
     public List<PhotoModel> getPhotos(){
